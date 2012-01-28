0176c2eef2edff6806ceb5346e720fddaccb4f54
Michael Pirog <mpirog@pirotekdc.com>
2012-01-27 21:38:23 -0500

    change template paths

--- puppet/manifests/params.pp
+++ puppet/manifests/params.pp
@@ -20,15 +20,15 @@ class puppet::params {
   
   case $operatingsystem {
     /(Debian|Ubuntu|Mint)/: {
-      $puppet_conf_template = "puppet/templates/puppet.debian.conf.erb"
+      $puppet_conf_template = "puppet/puppet.debian.conf.erb"
       $puppet_set_running = true
     }
     /(RedHat|CentOS|Fedora)/: {
-      $puppet_conf_template = "puppet/templates/puppet.fedora.conf.erb"
+      $puppet_conf_template = "puppet/puppet.fedora.conf.erb"
       $puppet_set_running = false
     }
     default: {
-      $puppet_conf_template = "puppet/templates/puppet.debian.conf.erb"
+      $puppet_conf_template = "puppet/puppet.debian.conf.erb"
       $puppet_set_running = false
     }
   }
