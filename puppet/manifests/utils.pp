f5a59327656ced75b52f349e306735e529452f8f
Michael Pirog <mpirog@pirotekdc.com>
2012-02-02 10:34:28 -0500

    add puppet:utils

---
+++ puppet/manifests/utils.pp
@@ -0,0 +1,16 @@
+# Class: puppet::utils
+#
+# Contains some utilities for the puppet module
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#   class puppet
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class puppet::utils {
+}
