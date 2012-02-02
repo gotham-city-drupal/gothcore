98e130d5e3f4ccea6df514a0c8083819cca9a125
Michael Pirog <mpirog@pirotekdc.com>
2012-02-02 11:15:59 -0500

    adding admin module

---
+++ admin/manifests/augeas.pp
@@ -0,0 +1,17 @@
+# Class: admin::augeas
+#
+# This module manages admin
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#   include admin::augeas
+#
+# [Remember: No empty lines between comments and class definition]
+class admin::augeas {
+  
+}
---
+++ admin/manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: admin
+#
+# This module manages admin
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class admin {
+
+
+}
