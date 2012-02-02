a034d0b0dd02733d11cbab425cff8f8fc6e84cc6
Michael Pirog <mpirog@pirotekdc.com>
2012-02-02 11:21:33 -0500

    adding augeas config

--- admin/manifests/augeas.pp
+++ admin/manifests/augeas.pp
@@ -13,5 +13,7 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class admin::augeas {
-  
+  package { [ "augeas-lenses", "augeas-tools", "libaugeas0", "libaugeas-ruby1.8" ]:
+    ensure => "present"
+  }
 }
